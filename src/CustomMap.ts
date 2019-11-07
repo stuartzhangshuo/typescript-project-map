201d8857659027de374c48b8b5007231774d8fb8
Stuart Zhang <shuozhang.ut@gmail.com>
2019-11-06 23:27:11 -0600

    reversed dependency of user and company from custom map; created mappable interface; app wrap up;

--- src/CustomMap.ts
+++ src/CustomMap.ts
@@ -1,5 +1,13 @@
-import { User } from "./User";
-import { Company } from "./Company";
+// Instructions to every other class
+// on how they can be an argument to "addMarker"
+export interface Mappable {
+  location: {
+    lat: number;
+    lng: number;
+  };
+  markerContent(): string;
+  color: string;
+}
 
 export class CustomMap {
   private googleMap: google.maps.Map;
@@ -14,13 +22,21 @@ export class CustomMap {
     });
   }
 
-  addMarker(mappable: User | Company): void {
-    new google.maps.Marker({
+  addMarker(mappable: Mappable): void {
+    const marker = new google.maps.Marker({
       map: this.googleMap,
       position: {
         lat: mappable.location.lat,
         lng: mappable.location.lng
       }
+    });
+
+    marker.addListener("click", () => {
+      const infoWindow = new google.maps.InfoWindow({
+        content: mappable.markerContent()
+      });
+
+      infoWindow.open(this.googleMap, marker);
     });
   }
 }
